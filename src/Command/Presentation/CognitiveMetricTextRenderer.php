35b9bb9592556678fb5238a0255b5bad1492d929
Florian Krämer <f.kraemer@clipmyhorse.tv>
2024-09-07 11:28:46 +0200

    Refactoring the metrics table renderer

--- src/Command/Presentation/CognitiveMetricTextRenderer.php
+++ src/Command/Presentation/CognitiveMetricTextRenderer.php
@@ -14,51 +14,68 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CognitiveMetricTextRenderer
 {
+    protected float $scoreThreshold = 0.5;
+
+    /**
+     * @var array<string>
+     */
+    protected array $keys = [
+        'lineCount',
+        'argCount',
+        'returnCount',
+        'variableCount',
+        'propertyCallCount',
+        'ifCount',
+        'ifNestingLevel',
+        'elseCount',
+    ];
+
+    /**
+     * @var array<string>
+     */
+    protected array $tableHeaders = [
+        "Method Name",
+        "Lines",
+        "Arguments",
+        "Returns",
+        "Variables",
+        "Property\nAccesses",
+        "If",
+        "If Nesting\nLevel",
+        "Else",
+        "Cognitive\nComplexity"
+    ];
+
     public function render(CognitiveMetricsCollection $metricsCollection, OutputInterface $output): void
     {
         $groupedByClass = $metricsCollection->groupBy('class');
 
         foreach ($groupedByClass as $className => $metrics) {
             $output->writeln("<info>Class: $className</info>");
-
-            $table = new Table($output);
-            $table->setStyle('box');
-            $table->setHeaders($this->getTableHeaders());
-
-            $rows = [];
-            foreach ($metrics as $metric) {
-                $row = $this->prepareTableRow($metric);
-                $rows[] = $row;
-            }
-
-            $table->setRows($rows);
-            $table->render();
+            $this->renderTable($output, $metrics);
             $output->writeln("");
         }
     }
 
-    /**
-     * @return string[]
-     */
-    protected function getTableHeaders(): array
+    protected function renderTable(OutputInterface $output, CognitiveMetricsCollection $metricsCollection): void
     {
-        return [
-            "Method Name",
-            "Lines",
-            "Arguments",
-            "Returns",
-            "Variables",
-            "Property\nAccesses",
-            "If",
-            "If Nesting\nLevel",
-            "Else",
-            "Cognitive\nComplexity"
-        ];
+        $table = new Table($output);
+        $table->setStyle('box');
+        $table->setHeaders($this->tableHeaders);
+
+        $rows = [];
+        foreach ($metricsCollection as $metric) {
+            $rows[] = $this->prepareTableRow($metric);
+            ;
+        }
+
+        $table->setRows($rows);
+        $table->render();
     }
 
     /**
      * @param CognitiveMetrics $metrics
-     * @return array<string, mixed>
+     * @return array<string, mixed>se
      */
     protected function prepareTableRow(CognitiveMetrics $metrics): array
     {
@@ -72,21 +89,20 @@ class CognitiveMetricTextRenderer
             'ifCount' => $metrics->getIfCount(),
             'ifNestingLevel' => $metrics->getIfNestingLevel(),
             'elseCount' => $metrics->getElseCount(),
-            'score' => $metrics->getScore() > 0.5 ? '<error>' . $metrics->getScore() . '</error>' : '<info>' . $metrics->getScore() . '</info>',
+            'score' => $metrics->getScore() > $this->scoreThreshold ? '<error>' . $metrics->getScore() . '</error>' : '<info>' . $metrics->getScore() . '</info>',
         ];
 
-        $keys = [
-            'lineCount',
-            'argCount',
-            'returnCount',
-            'variableCount',
-            'propertyCallCount',
-            'ifCount',
-            'ifNestingLevel',
-            'elseCount',
-        ];
+        return $this->formatValues($row, $metrics);
+    }
 
-        foreach ($keys as $key) {
+    /**
+     * @param array<string, mixed> $row
+     * @param CognitiveMetrics $metrics
+     * @return array<string, mixed>
+     */
+    protected function formatValues(array $row, CognitiveMetrics $metrics): array
+    {
+        foreach ($this->keys as $key) {
             $getMethod = 'get' . $key;
             $getMethodWeight = 'get' . $key . 'Weight';
             $weight = $metrics->{$getMethodWeight}();
