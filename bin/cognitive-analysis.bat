00862bec716651baa1667fcdd37be68a081c047c
Florian Krämer <fk@kreative-design.net>
2024-09-22 00:25:18 +0200

    Fixing bin file

---
+++ bin/cognitive-analysis.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Cognitive Code Analysis
+REM
+REM @author    Florian Krämer
+REM @copyright 2024 Florian Krämer
+REM @license   https://github.com/Phauthentic/cognitive-code-analysis/blob/master/LICENSE GPL-3.0
+
+if "%PHP_PEAR_PHP_BIN%" neq "" (
+    set PHPBIN=%PHP_PEAR_PHP_BIN%
+) else set PHPBIN=php
+
+"%PHPBIN%" "%~dp0\cognitive-analysis" %*
