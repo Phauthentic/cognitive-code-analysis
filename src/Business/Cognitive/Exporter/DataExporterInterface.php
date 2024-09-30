e040a5b80d7c963d813eb7a6dad345dc0a3ca468
Florian Krämer <fk@kreative-design.net>
2024-09-30 21:01:04 +0200

    Fixing the interface declaration

--- src/Business/Cognitive/Exporter/DataExporterInterface.php
+++ src/Business/Cognitive/Exporter/DataExporterInterface.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Phauthentic\CodeQualityMetrics\Business\Cognitive\Exporter;
 
+use Phauthentic\CodeQualityMetrics\Business\Cognitive\CognitiveMetricsCollection;
+
 /**
  *
  */
 interface DataExporterInterface
 {
-    //public function export(array $metrics, string $filename): void;
+    public function export(CognitiveMetricsCollection $metrics, string $filename): void;
 }
