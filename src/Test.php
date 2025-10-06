df57df490b3d207a6a7c7f9fafe212af30a03031
Florian Krämer <fk@kreative-design.net>
2025-10-06 12:38:12 +0200

    Test file to trigger the action

---
+++ src/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phauthentic\CognitiveCodeAnalysis;
+
+/**
+ *
+ */
+class Application
+{
+    public function run(): void
+    {
+        if (true === true) {
+            echo "Hello, World!";
+        } else {
+            echo "Goodbye, World!";
+            if (false === false) {
+                echo "Nested condition met.";
+                if (1 + 1 === 2) {
+                    echo "Math still works.";
+                }
+            }
+        }
+    }
+}
